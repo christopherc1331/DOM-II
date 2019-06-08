3b5acc78ac63d24ab19a069811b13f4b06cab36c
Chris Cruz <christopherc1331@gmail.com>
2019-06-07 19:00:30 -0700

    Added 3 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+let headNav = document.querySelectorAll("nav a");
+
+headNav.forEach(item => {
+  item.addEventListener("mouseover", e => {
+    e.target.style.fontSize = "2.5rem";
+  });
+  item.addEventListener("mouseout", e => {
+    e.target.style.fontSize = "1.6rem";
+  });
+});
+
+let btns = document.querySelectorAll(".btn");
+
+btns.forEach(item => {
+  item.addEventListener("dblclick", e => {
+    alert("EMAIL: fakeEmail@fakeEmailHost.com FOR MORE INFO!!!!");
+  });
+});
